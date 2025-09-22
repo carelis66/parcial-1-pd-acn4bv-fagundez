744ba3cd9dd0f270250604feb790fb92e8aaccfe
Carelis Fagúndez <fagundez.carelis@gmail.com>
2025-09-22 15:26:44 -0300

    chore: estructura inicial del spa  (Sin imagenes y meti archivo JS con una  class)

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ class Turno {
         this.estado = "reservado";
     }
 
-    // Método: devuelve un resumen del turno
+    // Para ver un resumen del turno
     resumen() {
         return `Turno ${this.id} - ${this.mascota} (${this.servicio}) con ${this.cliente} el ${this.fecha} a las ${this.hora}`;
     }
 }
 
-// Ejemplo de uso (para probar en consola)
+// Para ver si la consola anda (BORRAR despues)
 const turnoEjemplo = new Turno(1, "2025-09-20", "10:00", "Baño Premium", "Firulais", "Carelis");
 console.log(turnoEjemplo.resumen());
